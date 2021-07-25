9a1320aff4449a754ae6d1573767b45b42b6877a
Peter Ayeni <ayenicology@gmail.com>
2021-07-25 10:31:36 +0100

    add write keys to environment variable

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import * as snippet from '@segment/snippet';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
