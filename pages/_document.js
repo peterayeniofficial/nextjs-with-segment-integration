52e97857fb7894581095ff2b51f25afc91ff89c4
Peter Ayeni <ayenicology@gmail.com>
2021-07-25 10:45:16 +0100

    integrate segment

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import * as snippet from '@segment/snippet';
 
+const { ANALYTICS_WRITE_KEY } = process.env;
+
 class MyDocument extends Document {
+  /*
+    *
+    https://github.com/vercel/next.js/tree/canary/examples/with-segment-analytics
+    */
+
+  renderSnippet() {
+    const opts = {
+      apiKey: ANALYTICS_WRITE_KEY,
+      // note: the page option only covers SSR tracking.
+      // Page.js is used to track other events using `window.analytics.page()`
+      page: true,
+    };
+
+    if (NODE_ENV === 'development') {
+      return snippet.max(opts);
+    }
+
+    return snippet.min(opts);
+  }
+
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
@@ -10,7 +32,10 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          {/* Inject the Segment snippet into the <head> of the document  */}
+          <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
